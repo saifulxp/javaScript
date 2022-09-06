d03d5674ca31fce237cd7a855baac144b298cc8c
saifulxp <midulahamed999@gmail.com>
2022-09-06 15:42:13 +0600

    2nd

---
+++ myScript.js
@@ -0,0 +1,9 @@
+function myfunction(){
+  document.getElementById("ii").innerHTML = "Text Just Replace";
+}
+
+function hiHelo()
+{
+  document.getElementById("k1").innerHTML = "Hello Dolly!";
+  document.getElementById("k2").innerHTML = "How are you?";
+}
